84308ac240922d756781bbca96ef95ec931e7fe8
Triantafyllos Paschaleris <pashaler@hotmail.com>
2016-02-04 15:34:40 +0200

    properly handling the wildcard characters, added calculateWordValue

--- find_word.py
+++ find_word.py
@@ -1,11 +1,18 @@
 __author__ = 'Triantafyllos Paschaleris'
 
-from letters import lettersInHand, lettersPlayed
+from letters import lettersInHand, lettersPlayed, lettersValues
 from prepare_words_list import filename
 
 lettersInHand = ["Α", "Β", "Γ", "Ο", "*"] #debugging
 
 maxWordLength = len(lettersInHand) # TODO: adjust it for longer legth because of extra letters from on-board
+
+
+def calculateWordValue(_word=""):
+    #the word is expected in format "ΑΒΓΟ.*" where the letters after the "." are those replaced by "*"  e.g. "ΑΒ*Ο.Γ"
+    return sum(lettersValues[_letter] for _letter in word.split(".")[0])
+
+
 
 wordList = []
 with open(filename ,mode='r', encoding="utf-8") as f:
@@ -15,12 +22,15 @@ sorted(wordList, key=len) # Although the file is already sorted, in order to be 
 
 for word in wordList:
     if len(word) > maxWordLength: break # Since the list is sorted by the words length, then we don't have to look further.
-    _lettersInHand = lettersInHand[:]# Create a new copy with the [:] instead of a reference, because of latter .remove() method
+    _lettersInHand = lettersInHand[:]# Create a new copy with the [:] instead of a reference, because of the latter .remove() method
     for letter in word:
         if letter == "*": continue # "*" is the empty (wildcard) square
         if letter not in _lettersInHand:
-            break
+            if "*" in _lettersInHand:
+                word = word.replace(letter,"*",1) + "."+letter
+                _lettersInHand.remove("*") # "*" is the empty (wildcard) square, if we have one we use it now for this letter!
+            else: break
         else:
             _lettersInHand.remove(letter)
     else: # None of the break condition are met, so the word does match!
-        print(word)#debugging
+        print(word, calculateWordValue(word))#debugging
